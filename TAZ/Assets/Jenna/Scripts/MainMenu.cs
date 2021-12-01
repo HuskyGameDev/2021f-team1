8ede1364c2f3ad3dec45bf11731779f958a02fea
JRobben <jprobben@mtu.edu>
2021-12-01 15:00:45 -0500

    Main Menu
    main menu and option overlap on start up

--- TAZ/Assets/Jenna/Scripts/MainMenu.cs
+++ TAZ/Assets/Jenna/Scripts/MainMenu.cs
@@ -7,7 +7,13 @@ public class MainMenu : MonoBehaviour
 {
    
 public void PlayGame(){
+    Debug.Log("Play Game");
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+}
+
+public void QuitGame(){
+    Debug.Log("Quit Game");
+    Application.Quit();
 }
 
 }
