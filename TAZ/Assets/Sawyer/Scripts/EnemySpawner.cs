3076f20ed16d092625238568bf03e3aa9385351a
Sawyer Knowles <skdnowle@mtu.edu>
2021-12-01 19:47:56 -0500

    Enemy spawning script - 1

---
+++ TAZ/Assets/Sawyer/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public PlayerController player;
+    public GameObject enemyType1;
+    public GameObject enemyType2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
